41c41ba40be5422b07e668bed331ddc3083420f6
Maciej Aleksandrowicz <Maciej.Aleksandrow@enefit.pl>
2023-02-16 14:53:14 +0100

    Revert "removed docker image"
    This reverts commit ca90dc03c4a59dab2d33c76f20bce735322ba44d.

--- main.tf
+++ main.tf
@@ -9,3 +9,16 @@ terraform {
 
 provider "docker" {}
 
+resource "docker_image" "nginx" {
+  name         = "nginx:latest"
+  keep_locally = false
+}
+
+resource "docker_container" "nginx" {
+  image = docker_image.nginx.latest
+  name  = "from-atlantis"
+  ports {
+    internal = 80
+    external = 8000
+  }
+}
